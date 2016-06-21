4251a731192918b9db06f97bad0df8b3b5d35786
adeka <filatov.n@husky.neu.edu>
2016-06-21 02:48:05 -0400

    aw yeaa keyboardy

--- app/scripts/main.jsx
+++ app/scripts/main.jsx
@@ -1,38 +1,4 @@
-class Pitch extends React.Component {
-    constructor() {
-        super();
-        this.saw = new Wad({
-            source : 'sawtooth',
-        });
-        // this.state = {
-        // };
-        this.handleClick = this.handleClick.bind(this);
-        this.onKeyDown = this.onKeyDown.bind(this);
-        this.onKeyUp = this.onKeyUp.bind(this);
-    }
-    componentDidMount() {
-       $(document.body).on('keypress', this.onKeyDown);
-       $(document.body).on('keyup', this.onKeyUp);
 
-    }
-    componentWillUnmount() {
-    }
-
-    onKeyUp(e){
-
-    }
-    onKeyDown(e){
-
-    }
-    handleClick(e) {
-
-    }
-    render() {
-        return (
-            <div></div>
-        );
-    }
-}
 
 class xxx extends React.Component {
     constructor() {
@@ -98,7 +64,7 @@ class TimeDisplay extends React.Component {
     }
 }
 
-class Timeline extends React.Component {
+class Sequencer extends React.Component {
     constructor() {
         super();
         this.state = {
@@ -107,47 +73,148 @@ class Timeline extends React.Component {
         };
     }
     render() {
-        var w = $(this.refs.timeline).width();
+        this.timeScale = d3.scale.linear().domain([0, this.state.end]).range([0, this.props.w]);
+        $(this.refs.playhead).css("left", this.timeScale(this.props.elapsed));
+        this.scale = [
+          261.63,
+          293.66,
+          329.63,
+          349.23,
+          392,
+          440,
+          493.88
+        ]
+        var self = this;
+        return(
+            <div className="timelineContainer">
+                <div className="timeline">
+                    <div className="scrubber">
+                        <div ref="playhead" className="playhead fa fa-caret-down" />
+                    </div>
+                    {this.scale.map(function(result) {
+                        return <Notelane pitch={result} w={self.props.w} playing={self.props.playing} elapsed={self.props.elapsed}/>;
+                    })}
+
+                </div>
+            </div>
+        );
+    }
+}
+
+class Notelane extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            start : 0,
+            end: 300
+        };
+        this.notes = [];
+        this.addNote = this.addNote.bind(this);
+
+    }
+    addNote(time, duration){
+      this.notes.push({
+        style : {
+          width: this.timeScale(duration),
+          left: this.timeScale(time)
+        },
+        time : time,
+        duration: duration
+      })
+    }
+    render() {
+
+        this.timeScale = d3.scale.linear().domain([0, this.state.end]).range([0, this.props.w]);
+
+        var subs = 4;
         var bpm = 120;
         var meter = {top: 4, bottom: 4};
         var measureLength = (meter.top / (bpm / 60))*10;
-        var timeScale = d3.scale.linear().domain([0, this.state.end]).range([0, w]);
-        // var tickScale = d3.scale.linear().domain([0, tickCount]).range([0, w]);
-        $(this.refs.playhead).css("left", timeScale(this.props.elapsed));
-
+        var barLength = (meter.top / (bpm / 60))*10 / subs;
         var ticks = [];
         var barBuilder = 0;
-        for(var i = 0; i< this.state.end; i+= measureLength){
-            var h = 20;
-            //
-            // if(i % 4 == 0){
-            //     h = 20;
-            // }
-            // else{
-            //     h = 10;
-            // }
+        for(var i = 0; i< this.state.end; i+= barLength){
+            var cName;
+            if(Math.floor(i % (measureLength*meter.bottom)) ==  0){
+              cName = "bigTick";
+            }
+            else if(Math.floor(i % (measureLength)) ==  0){
+              cName = "tick";
+            }
+            else{
+              cName = "subTick";
+            }
+
             var tick = {
-                left : timeScale(barBuilder)+"px",
-                height: h,
-                width: timeScale(measureLength)+"px"
+              style : {
+                left : this.timeScale(i)+"px",
+                width: this.timeScale(barLength)+"px"
+              },
+              width : barLength,
+              cName : cName,
+              time : i
             }
             ticks.push(tick);
-            barBuilder += measureLength;
         }
+        var self = this;
         return(
-            <div ref="timeline" className="timeline">
-                <div className="scrubber">
-                    <div ref="playhead" className="playhead fa fa-caret-down" />
-                </div>
-                <div className="timescale">
-                {ticks.map(function(result) {
-                  //var left = result["left"];
-                    return <Bar subs={4} duration={timeScale(measureLength)} style={result} key={(result.left+'').hashCode()}/>;
-
-                  //return <Pitch ratio={ratio} details={details} key={ratio.hashCode()}/>;
+            <div ref="timeline" className="timescale">
+                {this.notes.map(function(result) {
+                    var left = self.timeScale(result.time);
+                    var width = self.timeScale(result.duration);
+                    return <Note pitch={self.props.pitch} playing={self.props.playing} duration={result.duration} time={result.time} elapsed={self.props.elapsed} left={left} width={width} style={result.style} />;
                 })}
-                </div>
+                {ticks.map(function(result) {
+                    return <Bar className={result.cName} time={result.time} addNote={self.addNote} width={result.width} duration={self.timeScale(barLength)} style={result.style} key={(result.style.left+'').hashCode()}/>;
+                })}
             </div>
+        );
+    }
+}
+
+class Note extends React.Component {
+    constructor() {
+        super();
+        this.saw = new Wad({
+            source : 'sawtooth',
+        });
+        // this.state = {
+        // };
+    }
+    play(){
+      this.saw.play({
+          volume  : .8,
+          wait    : 0,     // Time in seconds between calling play() and actually triggering the note.
+          loop    : false, // This overrides the value for loop on the constructor, if it was set.
+          pitch   : this.props.pitch,
+          label   : 'A',   // A label that identifies this note.
+          env     : {      // This is the ADSR envelope.
+              attack  : 0.1,  // Time in seconds from onset to peak volume.  Common values for oscillators may range from 0.05 to 0.3.
+              decay   : 0.0,  // Time in seconds from peak volume to sustain volume.
+              sustain : 1.0,  // Sustain volume level. This is a percent of the peak volume, so sensible values are between 0 and 1.
+              hold    : this.props.duration / 10, // Time in seconds to maintain the sustain volume level. If this is not set to a lower value, oscillators must be manually stopped by calling their stop() method.
+              release : .1     // Time in seconds from the end of the hold period to zero volume, or from calling stop() to zero volume.
+          },
+          panning : [1, -1, 10]
+      });
+    }
+    stop(){
+      if(this.saw.gain.length > 0){
+          this.saw.stop();
+      }
+    }
+    render() {
+      if(this.props.elapsed == this.props.time && this.props.playing){
+        this.play();
+      }
+      if(!this.props.playing){
+        this.stop();
+      }
+    //  console.log(elapsed + " " + this.prop.);
+      $(this.refs.self).css("left", this.props.left);
+      $(this.refs.self).width(this.props.width);
+        return (
+            <div ref="self" style={this.props.style} className="note"></div>
         );
     }
 }
@@ -155,22 +222,14 @@ class Timeline extends React.Component {
 class Bar extends React.Component {
   constructor() {
     super();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    this.props.addNote(this.props.time, this.props.width);
   }
   render(){
-    //this is fucking retarded haha
-    var subs = [];
-    for(var i = 0; i < this.props.subs; i++){
-      subs.push({
-        left : ((this.props.duration / this.props.subs) * i )+"px"
-      });
-    }
-    //this is some fuckin php shit
     return (
-      <div className="tick" style={this.props.style}>
-      {subs.map(function(result) {
-        return <div className="subTick" style={result}/>;
-      })}
-      </div>
+      <div className={this.props.className} style={this.props.style} onClick={this.handleClick} />
     );
   }
 }
@@ -180,7 +239,8 @@ class Toolbar extends React.Component {
         super();
         this.state = {
             elapsed : 0,
-            stopped : true
+            stopped : true,
+            playing : false
         };
         this.tick = this.tick.bind(this);
         this.playTimeline = this.playTimeline.bind(this);
@@ -193,10 +253,13 @@ class Toolbar extends React.Component {
                 this.start = new Date().getTime();
                 this.interval = setInterval(this.tick, 100);
                 this.setState({stopped : false});
+                this.setState({playing : true});
         }
         else{
             clearInterval(this.interval);
             this.setState({elapsed: 0});
+            this.setState({playing : false});
+
         }
     }
     componentWillUnmount() {
@@ -207,15 +270,16 @@ class Toolbar extends React.Component {
       this.setState({elapsed: elapsed});
     }
     render() {
+      var w = $(this.refs.panel).width();
       return (
            <div className="topToolbar">
-               <div className="info">
+               <div ref="panel" className="info">
                    <TimeDisplay elapsed={this.state.elapsed}/>
                    <div className="controls">
                         <PlayButton playTimeline={this.playTimeline} />
                    </div>
                 </div>
-               <Timeline elapsed={this.state.elapsed}/>
+                <Sequencer w={w} playing={this.state.playing} elapsed={this.state.elapsed}/>
            </div>
       );
     }
